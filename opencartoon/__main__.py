86e3bc3a1fb2354c43e294a8b4e550ce1710d144
benoit.lapointe.3@ens.etsmtl.ca <Benoit Lapointe>
2021-03-30 15:16:54 -0400

    Added upload function and cartoonify function skeleton.

--- opencartoon/__main__.py
+++ opencartoon/__main__.py
@@ -5,7 +5,6 @@ About this Module
 ------------------
 This module is the main entry point of The Main of the Image Cartoonifyer
 with OpenCV.
-
 """
 
 __author__ = "Benoit Lapointe"
@@ -13,17 +12,29 @@ __date__ = "2021-03-30"
 __copyright__ = "Copyright 2021, labesoft"
 __version__ = "1.0.0"
 
-import cv2 #for image processing
-import easygui #to open the filebox
-import numpy as np #to store image
-import imageio #to read image stored at particular path
-import sys
-import matplotlib.pyplot as plt
-import os
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import *
-from PIL import ImageTk, Image
+import easygui  # to open the filebox
+
+
+def cartoonify(ImagePath):
+    """All the operation will be done on the button click,
+
+    Thus, all the below steps are the part of this function
+
+    :param ImagePath: the path of the image to convert
+    """
+    pass
+
+def upload():
+    """Fileopenbox opens the box to choose file
+
+    Also help us store file path as string. The code opens the file
+    box, i.e the pop-up box to choose the file from the device, which opens
+    every time you run the code. fileopenbox() is the method in easyGUI
+    module which returns the path of the chosen file as a string.
+    """
+    ImagePath = easygui.fileopenbox()
+    cartoonify(ImagePath)
+
 
 if __name__ == '__main__':
     """Main entry point of opencartoon"""
