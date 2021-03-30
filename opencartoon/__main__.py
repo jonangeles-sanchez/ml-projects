8f2a85dcabace3eafd28c996387050cca47781c0
benoit.lapointe.3@ens.etsmtl.ca <Benoit Lapointe>
2021-03-30 15:19:50 -0400

    Added code to store the image.
    - Also added documentation on how cartoonifying images works

--- opencartoon/__main__.py
+++ opencartoon/__main__.py
@@ -4,7 +4,11 @@
 About this Module
 ------------------
 This module is the main entry point of The Main of the Image Cartoonifyer
-with OpenCV.
+with OpenCV. To convert an image to a cartoon, multiple transformations are
+done. Firstly, an image is converted to a Grayscale image. Then,
+the Grayscale image is smoothened, and we try to extract the edges in the
+image. Finally, we form a color image and mask it with edges. This creates a
+beautiful cartoon image with edges and lightened color of the original image.
 """
 
 __author__ = "Benoit Lapointe"
@@ -12,17 +16,36 @@ __date__ = "2021-03-30"
 __copyright__ = "Copyright 2021, labesoft"
 __version__ = "1.0.0"
 
+import sys
+
+import cv2
 import easygui  # to open the filebox
 
 
 def cartoonify(ImagePath):
     """All the operation will be done on the button click,
 
-    Thus, all the below steps are the part of this function
+    Thus, all the below steps are the part of this function. This code will
+    first convert our image into a numpy array.Imread is a method in cv2
+    which is used to store images in the form of numbers. This helps us to
+    perform operations according to our needs. The image is read as a numpy
+    array, in which cell values depict R, G, and B values of a pixel.
 
     :param ImagePath: the path of the image to convert
     """
-    pass
+    # read the image
+    originalmage = cv2.imread(ImagePath)
+    originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
+    # print(image)  # image is stored in form of numbers
+
+    # confirm that image is chosen
+    if originalmage is None:
+        print("Can not find any image. Choose appropriate file")
+        sys.exit()
+
+    ReSized1 = cv2.resize(originalmage, (960, 540))
+    # plt.imshow(ReSized1, cmap='gray')
+
 
 def upload():
     """Fileopenbox opens the box to choose file
