478637bf9944bed97b2eb3c9c6943b34d396d37e
Benoit Lapointe <benoit.lapointe@labesoft.ca>
2021-04-27 14:21:37 -0400

    Added Handwritten Recognition project

---
+++ writerecog/__init__.py
@@ -0,0 +1,14 @@
+"""The package of The Handwritten Recognition Application
+-----------------------------
+
+Project structure
+-----------------
+*writerecog/*
+    **__main__.py**:
+        The Main module of The Handwritten Recognition Application
+"""
+
+__author__ = "Benoit Lapointe"
+__date__ = "2021-04-27"
+__copyright__ = "Copyright 2021, Benoit Lapointe"
+__version__ = "1.0.0"
