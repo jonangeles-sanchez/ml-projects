dd2008f33c93cf06ed913410e7d15ecbc377a2ca
Benoit Lapointe <benoit.lapointe@labesoft.ca>
2021-05-27 08:13:44 -0400

    Created the cancerdetect package

---
+++ cancerdetect/__init__.py
@@ -0,0 +1,14 @@
+"""Breast Cancer Classification with Deep Learning
+-----------------------------
+
+Project structure
+-----------------
+*cancerdetect/*
+    **__init__.py**:
+        Breast Cancer Classification with Deep Learning
+"""
+
+__author__ = "Benoit Lapointe"
+__date__ = "2021-05-27"
+__copyright__ = "Copyright 2021, labesoft"
+__version__ = "1.0.0"
