d06e2d797feeb12775f1ae07b4e91eb8e0030e0b
Benoit Lapointe <benoit.lapointe@labesoft.ca>
2021-05-28 09:44:25 -0400

    Created the main module of the genderage package

---
+++ genderage/__main__.py
@@ -0,0 +1,24 @@
+"""The main of Gender and Age Detection with OpenCV
+-----------------------------
+
+Project structure
+-----------------
+*genderage/*
+    **__main__.py**:
+        The main of Gender and Age Detection with OpenCV
+
+About this Module
+------------------
+The goal of this module is to initiate the run of the Gender and Age Detection
+and define core components.
+"""
+
+__author__ = "Benoit Lapointe"
+__date__ = "2021-05-28"
+__copyright__ = "Copyright 2021, labesoft"
+__version__ = "1.0.0"
+
+
+if __name__ == '__main__':
+    """Main entry point of the genderage package"""
+    pass
