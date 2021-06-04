380c5c951348ebfeb8f2a9486c8e83e6d37acea7
Benoit Lapointe <benoit.lapointe@labesoft.ca>
2021-06-04 10:03:31 -0400

    Added coco library to our codebase.

---
+++ imsegment/lib/__init__.py
@@ -0,0 +1,18 @@
+"""
+-----------------------------
+
+Package structure
+-----------------
+*/*
+    **__init__.py**:
+        
+        
+About this module
+-----------------
+
+"""
+
+__author__ = "Benoit Lapointe"
+__date__ = "2021-06-01"
+__copyright__ = "Copyright 2021, Benoit Lapointe"
+__version__ = "1.0.0"
