efec37b9212228f931d0763f4b39861d9d047eda
Benoit Lapointe <benoit.lapointe@labesoft.ca>
2021-06-14 15:32:25 -0400

    Added a new package for the Human detection project (manproject)

---
+++ mandetect/__init__.py
@@ -0,0 +1,20 @@
+"""Real-time Human Detection & Counting
+-----------------------------
+
+Project structure
+-----------------
+*mandetect/*
+    **__init__.py**:
+        Real-time Human Detection & Counting
+
+About this Module
+------------------
+The goal of this module is to package this python project of Real-time Human
+Detection.
+"""
+
+__author__ = "Benoit Lapointe"
+__date__ = "2021-06-14"
+__copyright__ = "Copyright 2021, labesoft"
+__version__ = "1.0.0"
+
