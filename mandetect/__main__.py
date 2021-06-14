c4cdd8eaa0d3fc06a0637d9d625fa3e54a148aff
Benoit Lapointe <benoit.lapointe@labesoft.ca>
2021-06-14 15:36:37 -0400

    Added the main file of the project

---
+++ mandetect/__main__.py
@@ -0,0 +1,24 @@
+"""The main of the Real-time Human Detection & Counting
+-----------------------------
+
+Project structure
+-----------------
+*mandetect/*
+    **__main__.py**:
+        The main of the Real-time Human Detection & Counting
+
+About this Module
+------------------
+The goal of this module is to launch the mandetect application from the python
+package.
+"""
+
+__author__ = "Benoit Lapointe"
+__date__ = "2021-06-14"
+__copyright__ = "Copyright 2021, labesoft"
+__version__ = "1.0.0"
+
+
+if __name__ == '__main__':
+    """Main entry point of the mandetect package"""
+    pass
